1eefb05664025c7f535574cbb22d2be1c7487f26
HarlowMoran <hmoran72@smith.edu>
2025-03-31 20:57:29 -0400

    Library.Commit

--- Library.java
+++ Library.java
@@ -1,12 +1,58 @@
 /* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
+public class Library extends Building{
+
+  /**Attributes */
+  private Hashtable<String, Boolean> collection;
+
+    public Library(String name, String address, int nFloors, Hashtable<String, Boolean>collection) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<String, Boolean>(collection);
       System.out.println("You have built a library: 📖");
     }
-  
-    public static void main(String[] args) {
-      new Library();
+
+    public void addTitle(String title){
+      collection.put(title, true);
     }
+
+    public String removeTitle(String title){
+      collection.remove(title);
+      return title;
+    } 
+
+    public void checkOut(String title){
+      collection.replace(title, false);
+    }
+
+    public void returnBook(String title){
+      collection.replace(title, true);
+    }
+
+    public boolean containsTitle(String title){
+      if(collection.contains(title)){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+
+    public boolean isAvailable(String title){
+      if(collection.contains(true)){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+
+    public void printCollection(){
+      System.out.println(collection.toString());
+    }
+  
+//    public static void main(String[] args) {
+//      new Library();
+//    }
   
   }
